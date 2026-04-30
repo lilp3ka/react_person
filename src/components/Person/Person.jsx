51587ff9693e34d780783e47af81baf4d1130238
Andrey <andreiverpeka123@gmail.com>
2026-04-30 10:01:26 +0300

    Solution v2

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -7,18 +7,17 @@ export const Person = ({ person }) => {
   let partnerText = 'I am not married';
 
   if (isMarried) {
-    partnerText = sex === 'm'
-      ? `${partnerName} is my wife`
-      : `${partnerName} is my husband`;
+    partnerText =
+      sex === 'm'
+        ? `${partnerName} is my wife`
+        : `${partnerName} is my husband`;
   }
 
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
 
-      {age !== undefined && (
-        <p className="Person__age">{`I am ${age}`}</p>
-      )}
+      {age !== undefined && <p className="Person__age">{`I am ${age}`}</p>}
 
       <p className="Person__partner">{partnerText}</p>
     </section>
